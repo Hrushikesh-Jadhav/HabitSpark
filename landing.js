57eeebe139f3db7620761dd3e5ed02b97a093b50
Hrushikesh Jadhav <139479306+Hrushikesh-Jadhav@users.noreply.github.com>
2024-07-26 11:21:32 +0530

    Delete landing.js

--- landing.js
+++
@@ -1,9 +0,0 @@
-function navigateToSingupPage(){
-    window.location.href='login.html';
-}
-
-document.getElementsByClassName('btn').addEventListener('click',navigateToSingupPage);
-document.getElementById('myLink').addEventListener('click',function(event){
-    event.preventDefault();
-    navigateToSingupPage();
-});
