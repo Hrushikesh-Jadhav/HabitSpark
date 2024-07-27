48fefa223ae89028ca89b4e7496da158c5187085
Hrushikesh Jadhav <139479306+Hrushikesh-Jadhav@users.noreply.github.com>
2024-07-27 18:00:49 +0530

    Delete src directory

--- src/components/Progress.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Progress() {
-  return (
-    <div><h1>PROGRESS SECTION</h1></div>
-  )
-}
-
-export default Progress
--- src/components/TipVideos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function TipVideos() {
-  return (
-    <div><h1>TIPS/VIDEOS SECTION</h1></div>
-  )
-}
-
-export default TipVideos
